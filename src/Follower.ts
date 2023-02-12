5fa4bcf5d216292479b2e045ffc56779d11922a9
Jeremy-Vaud <100855862+Jeremy-Vaud@users.noreply.github.com>
2023-02-12 23:29:25 +0100

    followers

---
+++ src/Follower.ts
@@ -0,0 +1,32 @@
+export class Follower {
+    posX: number
+    posY: number
+    color: string = "brown"
+    size: number = 10
+    speed: number = 4
+
+    constructor(posX: number, posY: number) {
+        this.posX = posX
+        this.posY = posY
+    }
+
+    move(x: number, y: number) {
+        x = x - this.posX
+        y = -y + this.posY
+        let z = Math.atan(y / x)
+        if (z < 0 && y > 0) {
+            z = Math.PI + z
+        } else if (z > 0 && y < 0) {
+            z = Math.PI + z
+        } else if (z < 0 && x > 0) {
+            z = Math.PI * 2 + z
+        }
+        this.posX += this.speed * Math.cos(z)
+        this.posY += -this.speed * Math.sin(z)
+    }
+
+    draw(ctx: any): void {
+        ctx.fillStyle = this.color
+        ctx.fillRect(this.posX - this.size / 2, this.posY - this.size / 2, this.size, this.size);
+    }
+}
--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,17 @@
 import { Player } from "./Player";
+import { Follower } from "./Follower";
 
-const canvas : any = document.getElementById("canvas");
+const canvas: any = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 const player = new Player(canvas)
+const enemies: { follower: Follower[] } = { follower: [new Follower(300, 300),new Follower(500, 300),new Follower(700, 200)] }
 setInterval(() => {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    enemies.follower.forEach((enemy) => {
+        enemy.move(player.posX,player.posY)
+        enemy.draw(ctx)
+    })
     player.draw(ctx)
-}, 1000/60)
+}, 1000 / 60)
 
 ctx.fill
